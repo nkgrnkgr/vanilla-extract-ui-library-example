992dd77eddb68fd179851211b13af193c3f8def0
nkgrnkgr <beefupme@gmail.com>
2024-05-01 09:41:00 +0900

    vite iib mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), vanillaExtractPlugin()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			formats: ["umd"],
+			name: "reactTs",
+			fileName: "react-ts",
+		},
+	},
 });
