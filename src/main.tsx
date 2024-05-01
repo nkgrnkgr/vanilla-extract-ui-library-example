11f63fcad6b595f6c540dce99f888190a26dd15e
nkgrnkgr <beefupme@gmail.com>
2024-05-01 09:59:15 +0900

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Button } from "./components/Button";
 
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Button />
+    <Button>
+      <span>Click me</span>
+    </Button>
   </React.StrictMode>,
 );
